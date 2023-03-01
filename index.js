455e71262fb38ae2ca54defc7f779cffdd75b9c3
Frattolevrai <115535928+Frattolevrai@users.noreply.github.com>
2023-03-01 10:45:27 +0000

    modifiche alla pagina

--- index.js
+++
@@ -1,9 +0,0 @@
-// Nascondi il menu al caricamento della pagina
-$(document).ready(function() {
-    $("#menu").hide();
-  });
-  
-  // Mostra o nascondi il menu quando l'utente clicca sul pulsante "Menu"
-  $("#bottone-menu").click(function() {
-    $("#menu").toggle();
-  });
